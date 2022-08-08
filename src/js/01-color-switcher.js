01c406681f68560b4248f5cbfe88650cea58fe15
Oleksandr <71338915+superbowl-alex@users.noreply.github.com>
2022-08-08 15:28:26 +0300

    Task01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const refs = {
+  body: document.body,
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+let timerId = null;
 
+refs.btnStart.addEventListener('click', onClickStart);
+refs.btnStop.addEventListener('click', onClickStop);
+
+refs.btnStop.disabled = true;
+
+function onClickStart() {
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+  refs.body.style.backgroundColor = `${getRandomHexColor()}`;
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+}
+
+function onClickStop() {
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  const red = Math.round(Math.random(0, 1) * 255);
+  const green = Math.round(Math.random(0, 1) * 255);
+  const blue = Math.round(Math.random(0, 1) * 255);
+  return `rgb(${red},${green},${blue})`;
+}
