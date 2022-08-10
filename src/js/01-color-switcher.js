ddf556318cd3217b19bfd86bdea75203cc2d4e51
Oleksandr <71338915+superbowl-alex@users.noreply.github.com>
2022-08-10 14:14:42 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,15 +1,20 @@
+// create references to dom elements
 const refs = {
   body: document.body,
   btnStart: document.querySelector('button[data-start]'),
   btnStop: document.querySelector('button[data-stop]'),
 };
+
+// variable declaration
 let timerId = null;
 
+// add an eventListener to the start and stop buttons
 refs.btnStart.addEventListener('click', onClickStart);
 refs.btnStop.addEventListener('click', onClickStop);
 
 refs.btnStop.disabled = true;
 
+// function that is triggered when the start button is clicked
 function onClickStart() {
   refs.btnStart.disabled = true;
   refs.btnStop.disabled = false;
@@ -19,13 +24,14 @@ function onClickStart() {
   }, 1000);
 }
 
+// function that is triggered when the stop button is clicked
 function onClickStop() {
   refs.btnStart.disabled = false;
   refs.btnStop.disabled = true;
-
   clearInterval(timerId);
 }
 
+// function to generate random color
 function getRandomHexColor() {
   const red = Math.round(Math.random(0, 1) * 255);
   const green = Math.round(Math.random(0, 1) * 255);
